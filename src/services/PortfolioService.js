128de65103196a5dda465b85103e55d74d65a5bf
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-09-22 03:32:05 -0300

    feat: create public get portfolio routa

--- src/services/PortfolioService.js
+++ src/services/PortfolioService.js
@@ -2,16 +2,68 @@ import { StatusCodes } from "http-status-codes";
 import ApiError from "../ApiError/ApiError.js";
 
 export default class PortfolioService {
-  constructor(portfolioModel) {
+  constructor(
+    portfolioModel,
+    professionalService,
+    personalDataService,
+    softSkillsService,
+    hardSkillsService,
+    academicService,
+    projectService
+  ) {
     this.model = portfolioModel;
+    this.professionalService = professionalService;
+    this.personalDataService = personalDataService;
+    this.softSkillsService = softSkillsService;
+    this.hardSkillsService = hardSkillsService;
+    this.academicService = academicService;
+    this.projectService = projectService;
+  }
+
+  async getPortfolio(pathname) {
+    const pathnameFound = await this.model.findOne({ pathname });
+    if (!pathnameFound) {
+      throw new ApiError({
+        status: StatusCodes.NOT_FOUND,
+        message: "Not found pathname"
+      });
+    }
+
+    const { userId } = pathnameFound;
+
+    const professional = await this.professionalService.getByUserId(userId);
+    const personalDataInfo = await this.personalDataService.getByUserId(userId);
+    const softSkills = await this.softSkillsService.getByUserId(userId);
+    const hardSkills = await this.hardSkillsService.getByUserId(userId);
+    const academic = await this.academicService.getByUserId(userId);
+    const projects = await this.projectService.getByUserId(userId);
+
+    const { userId: _, _id, ...personalData } = personalDataInfo;
+
+    return {
+      professional: professional?.info ?? null,
+      personalData,
+      softSkills: softSkills?.info ?? null,
+      hardSkills: hardSkills?.info ?? null,
+      academic: academic?.info ?? null,
+      projects: projects?.info ?? null
+    };
   }
 
   async getPathNameByUserId(userId) {
-    this.model.findOne({ userId });
+    const pathname = await this.model.findOne({ userId });
+    if (!pathname) {
+      throw new ApiError({
+        status: StatusCodes.NOT_FOUND,
+        message: "Not found pathname"
+      });
+    }
+
+    return pathname;
   }
 
   async createByUserId(userId, pathname) {
-    const exists = await this.getPathNameByUserId(userId);
+    const exists = await this.model.findOne({ userId });
     if (exists) {
       throw new ApiError({
         status: StatusCodes.CONFLICT,
