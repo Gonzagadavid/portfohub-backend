c735786465797b8325d9b9a6b4c295f53518aed7
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-10-07 00:09:01 -0300

    feat: create portfolio routes

--- src/controllers/PortfolioController.js
+++ src/controllers/PortfolioController.js
@@ -7,6 +7,10 @@ export default class PortfolioController {
     this.getPortfolio = this.getPortfolio.bind(this);
     this.addOrUpdateTemplate = this.addOrUpdateTemplate.bind(this);
     this.getPortfolioInfo = this.getPortfolioInfo.bind(this);
+    this.getCheckInfo = this.getCheckInfo.bind(this);
+    this.getPathname = this.getPathname.bind(this);
+    this.getDataPortfolio = this.getDataPortfolio.bind(this);
+    this.pathnameIsUsed = this.pathnameIsUsed.bind(this);
   }
 
   async createPathname(request, response, next) {
@@ -15,7 +19,6 @@ export default class PortfolioController {
         body: { pathname },
         user: { id }
       } = request;
-      console.log(pathname);
       await this.service.createByUserId(id, pathname);
       response.status(StatusCodes.CREATED).end();
     } catch (error) {
@@ -55,6 +58,57 @@ export default class PortfolioController {
       const portfolioInfo = await this.service.getPortfolioInfo(id);
 
       response.status(StatusCodes.OK).json({ portfolioInfo });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getCheckInfo(request, response, next) {
+    try {
+      const {
+        user: { id }
+      } = request;
+      const checkInfo = await this.service.getCheckInfo(id);
+
+      response.status(StatusCodes.OK).json({ checkInfo });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async pathnameIsUsed(request, response, next) {
+    try {
+      const { pathname } = request.params;
+
+      const isUsed = await this.service.pathnameIsUsed(pathname);
+      response.status(isUsed ? StatusCodes.CONFLICT : StatusCodes.OK).end();
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  async getPathname(request, response, next) {
+    try {
+      const {
+        user: { id }
+      } = request;
+      const { pathname } = await this.service.getPathnameByUserId(id);
+      response.status(StatusCodes.OK).json({ pathname });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  async getDataPortfolio(request, response, next) {
+    try {
+      const {
+        user: { id }
+      } = request;
+      const portfolio = await this.service.getPortfolioByUserId(id);
+
+      response.status(StatusCodes.OK).json({ portfolio });
     } catch (error) {
       next(error);
     }
