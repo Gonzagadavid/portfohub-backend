ec428c14001dc57989ef41d1a1421b95f1233093
Gonzagadavid <gonzagadaviddev@gmail.com>
2024-11-03 16:57:54 -0300

    feat: create portfolio integration test

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,7 +1,17 @@
 export default {
   clearMocks: true,
-  //   collectCoverage: true,
-  //   coverageDirectory: "coverage",
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coveragePathIgnorePatterns: ["/app/", "/node_modules/", "/tests/"],
+  coverageReporters: ['clover', 'json', 'lcov', 'html',['text', {skipFull: true}]],
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 30,
+      lines: 50,
+      statements: 50,
+    },
+  },
   coverageProvider: "v8",
   transform: {},
   setupFiles: ["<rootDir>/tests/setup-env.js"]
